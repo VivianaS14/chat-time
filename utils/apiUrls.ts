01e89d1058020f430cfe30110c756f890408c66a
VivianaS14 <vasorianp@gmail.com>
2024-01-23 11:47:27 -0500

    Api helper for URLs

---
+++ utils/apiUrls.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = "http://192.168.10.21:8000";
+
+export const api = axios.create({
+  baseURL: API_URL,
+});
+
+export const apiUrls = {
+  register: "/register",
+};
