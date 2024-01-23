3a7ef0650125e12c1ec39090d489e85efee5a128
VivianaS14 <vasorianp@gmail.com>
2024-01-23 08:15:59 -0500

    Custom input use react hook form

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -7,16 +7,50 @@ import {
   View,
 } from "react-native";
 import { Colors } from "../../utils/Colors";
+import {
+  Control,
+  Controller,
+  UseControllerProps,
+  useController,
+} from "react-hook-form";
+import { FormValues } from "../../types/Login";
 
 interface Props extends TextInputProps {
-  name: string;
+  label: string;
+  name: keyof FormValues;
+  control: Control<FormValues>;
+  error?: string;
+  requiredMessage?: string;
+  regex?: RegExp;
 }
 
 function CustomInput(props: Props) {
+  const { label, control, name, error } = props;
+
   return (
     <View style={style.inputContainer}>
-      <Text style={style.text}>{props.name}</Text>
-      <TextInput {...props} style={style.input} />
+      <Text style={[style.text, error ? { color: Colors.error } : {}]}>
+        {label}
+      </Text>
+      <Controller
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            value={field.value}
+            onChangeText={field.onChange}
+            style={[
+              style.input,
+              error ? { borderBottomColor: Colors.error } : {},
+            ]}
+            {...props}
+          />
+        )}
+        name={name}
+        rules={{
+          required: props.requiredMessage,
+        }}
+      />
+      {error && <Text style={style.error}>{error}</Text>}
     </View>
   );
 }
@@ -34,5 +68,9 @@ const style = StyleSheet.create({
   },
   text: {
     color: Colors.gray,
+  },
+  error: {
+    color: Colors.error,
+    fontSize: 13,
   },
 });
