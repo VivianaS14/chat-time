394c9c9a074bab692dcfcec9ad895d219f2409e1
VivianaS14 <vasorianp@gmail.com>
2024-01-23 11:47:48 -0500

    USer & Message models

---
+++ api/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// User model
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  friendRequest: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  sentFriendRequest: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
